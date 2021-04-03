84c14712bd7aef17e11b48445047e39b19dd33ef
Anastasia-Troian <56818476+Anastasia-Troian@users.noreply.github.com>
2021-04-03 21:29:27 +0300

    Add program

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {createStore} from 'redux'
+import './App.css';
+import Counter from './Components/counter/counter';
+
+function App() {
+  return (
+    <div>Hello <Counter/></div>
+  );
+}
+
+export default App;
